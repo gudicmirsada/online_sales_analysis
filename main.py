2b7030fc4b5d236bbb71675414c8131af750e250
Mirsada Gudic <mir.sada@gmx.de>
2026-02-08 12:23:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
 from product import Product
 from product_manager import ProductManager
+from cart import Cart
 
 def main():
+    # Kreiranje ProductManager instance
     manager = ProductManager()
 
+    # Kreiranje proizvoda
     product1 = Product("Laptop", 1200, 5)
     product2 = Product("Mis", 25, 20)
     product3 = Product("Tastatura", 80, 10)
 
+    # Dodavanje proizvoda u ProductManager
     manager.add_product(product1)
     manager.add_product(product2)
     manager.add_product(product3)
 
-    manager.display_products()
-    print("Total inventory value:", manager.total_inventory_value())
+    # Kreiranje instance Cart
+    cart = Cart()
+
+    # Dodavanje proizvoda u korpu
+    cart.add_to_cart(product1)
+    cart.add_to_cart(product2)
+    cart.add_to_cart(product3)
+
+    # Prikaz korpe i ukupne vrijednosti
+    cart.display_cart()
+    print("Ukupna vrijednost korpe:", cart.total_cart_value())
 
 if __name__ == "__main__":
     main()
