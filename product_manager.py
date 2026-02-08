e5640eeda8607fdc342ed07e0cd93da368f7f174
Mirsada Gudic <mir.sada@gmx.de>
2026-02-08 11:58:06 +0100

    Add product removal by name

--- product_manager.py
+++ product_manager.py
@@ -16,3 +16,9 @@ class ProductManager:
         for product in self.products:
             total += product.price * product.quantity
         return total
+    def remove_product_by_name(self, name):
+        for product in self.products:
+            if product.name == name:
+                self.products.remove(product)
+                return True
+        return False
